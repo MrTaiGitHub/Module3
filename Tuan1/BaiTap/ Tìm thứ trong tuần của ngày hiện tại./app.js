8641b48d7cb9caf8d3a2e88aeabcf0200893412b
MrTaiGitHub <108655185+MrTaiGitHub@users.noreply.github.com>
2022-07-07 08:40:44 +0700

    Delete Tuan1/BaiTap directory

--- Tuan1/BaiTap/ Tìm thứ trong tuần của ngày hiện tại./app.js
+++
@@ -1,9 +0,0 @@
-let Array = ['Chủ nhật','Thứ 2','Thứ 3','Thứ 4','Thứ 5','Thứ 6','Thứ 7']
-
-function getDay(str) {
-
-    let date = new Date(str)
-
-    console.log(Array[date.getDay()])
-}
-getDay("2022-07-04")
