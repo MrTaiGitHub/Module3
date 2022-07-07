677d18b18b9489da3a24bf6e52d1196383088ef8
MrTaiGitHub <108655185+MrTaiGitHub@users.noreply.github.com>
2022-07-07 08:39:04 +0700

    Delete BaiTap directory

--- BaiTap/Sử dụng axios để lấy dữ liệu từ API 2/app.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-async function getPictureOfDay() {
-    let picture = await axios.get('https://jsonplaceholder.typicode.com/users')
-    return picture.data
-}
-getPictureOfDay().then(result => {
-    console.log(result)
-})
